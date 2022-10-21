2551853b7c3b8fcb6bfb0ae6ca2225bcda8eed40
edsonarios <edsonrios9@gmail.com>
2022-10-20 22:23:46 -0400

    Added list, button togle

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'front-end';
+
+  title = 'NodeJs with Arduino';
+  users: { name: string, title: string }[] = [
+    { name: 'Carla Espinosa', title: 'Nurse' },
+    { name: 'Bob Kelso', title: 'Doctor of Medicine' },
+    { name: 'Janitor', title: 'Janitor' },
+    { name: 'Perry Cox', title: 'Doctor of Medicine' },
+    { name: 'Ben Sullivan', title: 'Carpenter and photographer' },
+  ];
+  OnOff = false;
+  control() {
+    this.OnOff = !this.OnOff
+    console.log("aqui1", this.OnOff)
+  }
+  control2() {
+    this.OnOff = !this.OnOff
+    console.log("aqui2", this.OnOff)
+  }
+
 }
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbThemeModule, NbSidebarModule, NbLayoutModule, NbButtonModule, NbListModule, NbUserModule, NbCardModule, NbIconModule, NbToggleModule, } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
@@ -17,7 +16,15 @@ import { NbEvaIconsModule } from '@nebular/eva-icons';
     BrowserAnimationsModule,
     NbThemeModule.forRoot({ name: 'default' }),
     NbLayoutModule,
-    NbEvaIconsModule
+    NbEvaIconsModule,
+    // RouterModule, // RouterModule.forRoot(routes, { useHash: true }), if this is your app.module
+    NbSidebarModule.forRoot(),
+    NbButtonModule,
+    NbListModule,
+    NbUserModule,
+    NbCardModule,
+    NbIconModule,
+    NbToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
